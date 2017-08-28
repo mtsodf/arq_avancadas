fda0875535d3929ca18bb6839f9376c5b67d59ec
Mateus Oliveira de Figueiredo <mateus@Mateuss-MacBook-Air.local>
2017-08-28 08:39:24 -0300

    Opt2 paralelizado com OpenMP

--- projeto_final/Opt2.cpp
+++ projeto_final/Opt2.cpp
@@ -1,22 +1,55 @@
 #include "Opt2.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include <omp.h>
+
 
 void findMiniMinj(Path* path, int *mini, int *minj, float* minchange){
-    int inext, jnext;
-    float change;
 
     *minchange = 0.0;
+    float * minchange_array;
+    int *mini_array, *minj_array;
+    #pragma omp parallel shared(minchange_array, mini_array, minj_array)
+    {
+        int inext, jnext;
+        float change;
+        int id = omp_get_thread_num();
+        int num_threads = omp_get_num_threads();
 
-    for(int i = 0; i < path->size-2;i++){
-        inext = (i+1)%(path->size);
-        for(int j = i+2; j < path->size; j++){
-            jnext = (j+1)%(path->size);
-            change = path->dist(i,j) + path->dist(inext, jnext) - path->dist(i,inext) - path->dist(j, jnext);
-            if(*minchange > change){
-                *minchange = change;
-                *mini = i;
-                *minj = j;
+        #pragma omp single
+        {
+            minchange_array = (float*)malloc(sizeof(float)*num_threads);
+            mini_array = (int*)malloc(sizeof(int)*num_threads);
+            minj_array = (int*)malloc(sizeof(int)*num_threads);
+            for(int i = 0; i < num_threads; i++) minchange_array[i] = 0.0;
+        }
+
+        #pragma omp for schedule(dynamic, 10)
+        for(int i = 0; i < path->size-2;i++){
+            inext = (i+1)%(path->size);
+            for(int j = i+2; j < path->size; j++){
+                jnext = (j+1)%(path->size);
+                change = path->dist(i,j) + path->dist(inext, jnext) - path->dist(i,inext) - path->dist(j, jnext);
+                if(minchange_array[id] > change){
+                    minchange_array[id] = change;
+                    mini_array[id] = i;
+                    minj_array[id] = j;
+                }
             }
+        }
+
+        #pragma omp single
+        {
+            for(int i = 0; i < num_threads; i++){
+                if(*minchange > minchange_array[i]){
+                    *minchange = minchange_array[i];
+                    *mini = mini_array[i];
+                    *minj = minj_array[i];
+                }
+            }
+            free(minchange_array);
+            free(mini_array);
+            free(minj_array);
         }
     }
 }
@@ -25,6 +58,7 @@ void findMiniMinj(Path* path, int *mini, int *minj, float* minchange){
 Opt2::Opt2(Path* path){
     this->path = path;
 }
+
 
 bool Opt2::solve(Path* path, bool log){
 
@@ -36,7 +70,7 @@ bool Opt2::solve(Path* path, bool log){
         findMiniMinj(path, &mini, &minj, &minchange);
         if(minchange < 0.0)  path->swapTotal(mini+1, minj);
         iters++;
+        if(iters%100==0) printf("Iteracao %d. Custo %f\n", iters, path->cost);
     } while(minchange < 0.0);
-
 
 }
--- projeto_final/tests/Opt2Test.cpp
+++ projeto_final/tests/Opt2Test.cpp
@@ -13,7 +13,63 @@
 using namespace std;
 
 
-TEST(Opt2, SolveTest){
+TEST(Opt2, SolveTest_48){
+    return;
+    vector<City*> cities;
+    vector<City*> cities_sol;
+    double start_time, end_time;
+    int num_threads;
+    cities = createCityVector("../../ALL_tsp/att48.tsp");
+    EXPECT_EQ(cities.size(), 48);
+    EXPECT_NEAR(cities[0]->x, 6734, 1e-6);
+    EXPECT_NEAR(cities[0]->y, 1453, 1e-6);
+
+    EXPECT_NEAR(cities[47]->x, 3023, 1e-6);
+    EXPECT_NEAR(cities[47]->y, 1942, 1e-6);
+
+    #pragma omp parallel
+    #pragma omp single
+    {
+        num_threads = omp_get_num_threads();
+        printf("Rodando com %d threads\n", num_threads);
+
+    }
+
+
+    printf("rand 1 %d\n", rand());
+    //printf("rand 2 %d\n", rand());
+    //printf("rand 3 %d\n", rand());
+
+    cities_sol = readSolution("../../ALL_tsp/att48.opt.tour", cities, cities.size());
+
+
+    Path *path_sol = new Path(cities_sol);
+
+    printf("Tamanho do caminho %d\n", cities.size());
+
+    Path* path = new Path(cities);
+
+    path->scramble();
+
+    Opt2* opt2Solve = new Opt2(path);
+
+    start_time = get_clock_msec();
+    opt2Solve->solve(opt2Solve->path, false);
+    end_time = get_clock_msec();
+
+    printf("Tempo para conseguir solucao %f\n", end_time-start_time);
+    printf("Solucao Calc %f\n", path->cost);
+    printf("Solucao      %f\n", path_sol->cost);
+
+    EXPECT_GT(path_sol->cost*1.10, path->cost);
+
+    PrintPath("path.txt", path);
+
+}
+
+TEST(Opt2, SolveTest_200){
+    return;
+    srand(time(NULL));
     vector<City*> cities;
     vector<City*> cities_sol;
     cities = createCityVector("../../ALL_tsp/att48.tsp");
@@ -42,22 +98,112 @@ TEST(Opt2, SolveTest){
     printf("Tamanho do caminho %d\n", cities.size());
 
     Path* path = new Path(cities);
-    //srand (time(NULL));
-    //path->scramble();
 
-
+    path->scramble();
 
     Opt2* opt2Solve = new Opt2(path);
 
     opt2Solve->solve(opt2Solve->path, false);
 
     printf("Solucao Calc %f\n", path->cost);
-    printf("Solucao %f\n", path_sol->cost);
+    printf("Solucao      %f\n", path_sol->cost);
 
-    EXPECT_NEAR(path_sol->cost*1.15, path->cost, 1e-5);
+    EXPECT_GT(path_sol->cost*1.10, path->cost);
 
+    PrintPath("path.txt", path);
+
+}
+
+
+TEST(Opt2, SolveTest_1379){
+    return;
+    //srand(time(NULL));
+    vector<City*> cities;
+    double start_time, end_time;
+    int num_threads;
+
+    #pragma omp parallel
+    #pragma omp single
+    {
+        num_threads = omp_get_num_threads();
+        printf("Rodando com %d threads\n", num_threads);
+
+    }
+
+    cities = createCityVector("../../ALL_tsp/nrw1379.tsp");
+    EXPECT_EQ(cities.size(), 1379);
+
+    EXPECT_NEAR(cities[0]->x, 2918, 1e-6);
+    EXPECT_NEAR(cities[0]->y, 6528, 1e-6);
+
+    EXPECT_NEAR(cities[1]->x, 2925, 1e-6);
+    EXPECT_NEAR(cities[1]->y, 6597, 1e-6);
+
+
+    Path* path = new Path(cities);
+
+    path->scramble();
+
+    Opt2* opt2Solve = new Opt2(path);
+
+    start_time = get_clock_msec();
+    opt2Solve->solve(opt2Solve->path, false);
+    end_time = get_clock_msec();
+    printf("Tempo para conseguir solucao %f\n", end_time-start_time);
+
+    printf("Solucao Calc %f\n", path->cost);
+
+    PrintPath("path.txt", path);
+
+}
+
+TEST(Opt2, SolveTest_442){
+    return;
+    //srand(time(NULL));
+    vector<City*> cities;
+    double start_time, end_time;
+    int num_threads;
+
+    #pragma omp parallel
+    #pragma omp single
+    {
+        num_threads = omp_get_num_threads();
+        printf("Rodando com %d threads\n", num_threads);
+
+    }
+
+    cities = createCityVector("../../ALL_tsp/pcb442.tsp");
+
+    EXPECT_EQ(cities.size(), 442);
+
+    EXPECT_NEAR(cities[0]->x, 2.00000e+02, 1e-6);
+    EXPECT_NEAR(cities[0]->y, 4.00000e+02, 1e-6);
+
+    EXPECT_NEAR(cities[441]->x, 0.00000e+00, 1e-6);
+    EXPECT_NEAR(cities[441]->y, 0.00000e+00, 1e-6);
+    Path* path = new Path(cities);
+
+    vector<City*> cities_sol;
+    cities_sol = readSolution("../../ALL_tsp/pcb442.opt.tour", cities, cities.size());
+    Path* path_sol = new Path(cities_sol);
+
+    path->scramble();
+
+    Opt2* opt2Solve = new Opt2(path);
+
+    start_time = get_clock_msec();
+    opt2Solve->solve(opt2Solve->path, false);
+    end_time = get_clock_msec();
+    printf("Tempo para conseguir solucao %f\n", end_time-start_time);
+
+
+    printf("Solucao Calc %f\n", path->cost);
+    printf("Solucao      %f\n", path_sol->cost);
+
+    EXPECT_GT(path_sol->cost*1.15, path->cost);
 
 
     PrintPath("path.txt", path);
 
 }
+
